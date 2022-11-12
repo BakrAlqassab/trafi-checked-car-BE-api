4c6ef8f3641a5a6890d2cf63ee12f1ba1c7c97f8
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2022-11-12 11:15:27 +0200

    create controllers and route files to control the api request

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// Here the entry point of the project 
+const express = require('express');
+const app = express();
+
+const postRoute = require('./routes/posts')
+// run the postRoute as middlewre, when the user visiting the below url prefix
+app.use("/posts",postRoute)
+module.exports = app
